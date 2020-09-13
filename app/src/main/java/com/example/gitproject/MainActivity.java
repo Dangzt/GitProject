369ab9359b0d84fc9eaeaf8de8facd1dca42cb8e
dangzhentao <dangzhentao@keyidabj.com>
2020-09-13 10:06:43 +0800

    dev分支代码

--- app/src/main/java/com/example/gitproject/MainActivity.java
+++ app/src/main/java/com/example/gitproject/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"第一次提交代码",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"dev分支开发",Toast.LENGTH_LONG).show();
     }
 }
