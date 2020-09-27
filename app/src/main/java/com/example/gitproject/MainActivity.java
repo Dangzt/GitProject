30760f20140246ab138c30be8688cba642ff689b
dangzhentao <dangzhentao@keyidabj.com>
2020-09-27 15:55:25 +0800

    重置

--- app/src/main/java/com/example/gitproject/MainActivity.java
+++ app/src/main/java/com/example/gitproject/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.gitproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: ");
     }
 }
